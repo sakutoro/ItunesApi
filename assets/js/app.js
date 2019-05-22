a0abccb0f33c456ebffa5a05ecf5b513525dfac1
sakutoro <s_kinjyo1904@pam.co.jp>
2019-05-22 13:37:05 +0900

    ajax 書き方修正

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,25 +2,26 @@ $(function() {
 
   $("#search-btn").on("click",function() {
     // 検索ボタンがクリックされたら
+    
+    $.ajax({
+      // データの通信をするところ
+      url:"https://itunes.apple.com/search",// 通信先URL
+      type:"GET", // GET送信 or POST送信
+      dataType: 'jsonp',// 検索結果の形式
+      data: {
+         term: "あいみょん",
+         country: "jp",
+      }
+
+   }).done( (data) => {
+      // 通信成功した時
+      console.log(data);
+
+   }).fail((error) => {
+      // 通信失敗した時
+      console.log(error);
+   })
   })
     // itunesに曲を検索しに行く(Ajax)
 
-    $.ajax({
-       // データの通信をするところ
-       url:"https://itunes.apple.com/search",// 通信先URL
-       type:"GET", // GET送信 or POST送信
-       dataType:"jsonp",// 検索結果の形式
-       data: {
-          term: "あいみょん",
-          country: "jp",
-       }
-
-    }).done( (data) => {
-       // 通信成功した時
-       console.log(data);
-
-    }).fail((error) => {
-       // 通信失敗した時
-       console.log(error);
-    })
 })
